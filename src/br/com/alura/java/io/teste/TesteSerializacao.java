82904eb50a51095c745419c38d448c46b5c00a51
Danilo Kenzo <dani.kenzo@hotmail.com>
2019-05-20 10:30:06 -0300

    Serializando objetos

--- src/br/com/alura/java/io/teste/TesteSerializacao.java
+++ src/br/com/alura/java/io/teste/TesteSerializacao.java
@@ -1,11 +1,24 @@
 package br.com.alura.java.io.teste;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class TesteSerializacao {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
-		String nome = "Danilo Kenzo";
+//		String nome = "Danilo Kenzo";
 		
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objecto.bin"));
+//		oos.writeObject(nome);
+//		oos.close();
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objecto.bin"));
+		String nome = (String) ois.readObject();
+		ois.close();
+		System.out.println(nome);
 	}
 
 }
