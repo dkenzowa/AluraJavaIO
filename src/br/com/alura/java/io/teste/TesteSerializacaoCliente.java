aee3d4d06ba46f34efb55ca9abbd85e8f6183882
Danilo Kenzo <dani.kenzo@hotmail.com>
2019-05-20 11:51:08 -0300

    Atualização

--- src/br/com/alura/java/io/teste/TesteSerializacaoCliente.java
+++ src/br/com/alura/java/io/teste/TesteSerializacaoCliente.java
@@ -1,27 +1,27 @@
 package br.com.alura.java.io.teste;
 
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class TesteSerializacaoCliente {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
-//		Cliente cliente = new Cliente();
-//		cliente.setNome("Danilo Kenzo");
-//		cliente.setProfissao("Analista de T.I");
-//		cliente.setCpf("34124314122");
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo Kenzo");
+		cliente.setProfissao("Analista de T.I");
+		cliente.setCpf("34124314122");
 		
-//		String nome = "Danilo Kenzo";
-//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
-//		oos.writeObject(cliente);
-//		oos.close();
+		String nome = "Danilo Kenzo";
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+		oos.writeObject(cliente);
+		oos.close();
 		
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
-		Cliente cliente = (Cliente) ois.readObject();
-		ois.close();
-		System.out.println(cliente.getNome());
+//		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+//		Cliente cliente = (Cliente) ois.readObject();
+//		ois.close();
+//		System.out.println(cliente.getNome());
 	}
 
 }
